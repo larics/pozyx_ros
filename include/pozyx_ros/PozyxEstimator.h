7ea054c9a7b8b355a537281a353acca504c415cb
suiauthon <carek.marko@gmail.com>
2019-05-09 14:57:33 +0200

    working parameters for pozyx

--- include/pozyx_ros/PozyxEstimator.h
+++ include/pozyx_ros/PozyxEstimator.h
@@ -2,16 +2,25 @@
 #define POZYX_ESTIMATOR
 
 #include <geometry_msgs/TransformStamped.h>
+#include "ros/ros.h"
+#include <pozyx_ros/KalmanFilter.h>
+#include <sensor_msgs/Imu.h>
 
 class PozyxEstimator
 {
 public:
 	PozyxEstimator(int rate);
 	void transformInputCbRos(const geometry_msgs::TransformStamped &msg);
+	void imucb(const sensor_msgs::Imu &imu);
 	void run(void);
 private:
-	int rate_;
-	bool new_measurement_;
+	ros::NodeHandle n;
+	ros::Subscriber transform_input_, imu_input;
+	ros::Publisher transform_estimated_pub_, imu_pub_;
+	int rate_, seq_;
+	bool new_measurement_, start_flag_;
+
+	KalmanFilter kf_x_, kf_y_, kf_z_;
 
 	geometry_msgs::TransformStamped raw_transform_;
 };
--- src/PozyxEstimator.cpp
+++ src/PozyxEstimator.cpp
@@ -1,11 +1,34 @@
 #include <pozyx_ros/PozyxEstimator.h>
-#include "ros/ros.h"
 
 PozyxEstimator::PozyxEstimator(int rate)
 	: rate_(rate),
-	  new_measurement_(false)
+	  new_measurement_(false),
+	  start_flag_(false),
+	  seq_(0)
 {
+	transform_input_ = n.subscribe("/eaglered/position", 1, &PozyxEstimator::transformInputCbRos, this);
+	imu_input = n.subscribe("/euroc3/imu", 1, &PozyxEstimator::imucb, this);
+	transform_estimated_pub_ = n.advertise<geometry_msgs::TransformStamped>("estimated_transform", 1);
+	imu_pub_ = n.advertise<sensor_msgs::Imu>("imu", 1);
 
+	kf_x_.setMeasureNoise(10.0);
+	kf_y_.setMeasureNoise(10.0);
+	kf_z_.setMeasureNoise(10.0);
+
+	kf_x_.setPositionNoise(0.1);
+	kf_y_.setPositionNoise(0.1);
+	kf_z_.setPositionNoise(0.1);
+
+	kf_x_.setVelocityNoise(10.0);
+	kf_y_.setVelocityNoise(10.0);
+	kf_z_.setVelocityNoise(10.0);
+}
+
+void PozyxEstimator::imucb(const sensor_msgs::Imu &imu)
+{
+	sensor_msgs::Imu imu1 = imu;
+	imu1.header.stamp = ros::Time::now();
+	imu_pub_.publish(imu1);
 }
 
 void PozyxEstimator::run()
@@ -16,27 +39,56 @@ void PozyxEstimator::run()
 	{
 		ros::spinOnce();
 
+		kf_x_.modelUpdate(1.0/rate_);
+		kf_y_.modelUpdate(1.0/rate_);
+		kf_z_.modelUpdate(1.0/rate_);
+
 		if (new_measurement_)
 		{
 			new_measurement_ = false;
+			kf_x_.measureUpdate(raw_transform_.transform.translation.x);
+			kf_y_.measureUpdate(raw_transform_.transform.translation.y);
+			kf_z_.measureUpdate(raw_transform_.transform.translation.z);
 		}
 
+		geometry_msgs::TransformStamped transform_estimated;
+
+		transform_estimated.header.stamp = ros::Time::now();
+		transform_estimated.header.frame_id = raw_transform_.header.frame_id;
+		transform_estimated.header.seq = ++seq_; 
+		transform_estimated.child_frame_id = raw_transform_.child_frame_id;
+
+		transform_estimated.transform.translation.x = kf_x_.getPosition();
+		transform_estimated.transform.translation.y = kf_y_.getPosition();
+		transform_estimated.transform.translation.z = kf_z_.getPosition();
+
+		transform_estimated.transform.rotation.x = raw_transform_.transform.rotation.x;
+		transform_estimated.transform.rotation.y = raw_transform_.transform.rotation.y;
+		transform_estimated.transform.rotation.z = raw_transform_.transform.rotation.z;
+		transform_estimated.transform.rotation.w = raw_transform_.transform.rotation.w;
+
+		transform_estimated_pub_.publish(transform_estimated);
 		loop_rate.sleep();
 	}
 }
 
 void PozyxEstimator::transformInputCbRos(const geometry_msgs::TransformStamped &msg)
 {
+	if (!start_flag_)
+	{
+		start_flag_ = true;
+		kf_x_.initializePosition(msg.transform.translation.x);
+		kf_y_.initializePosition(msg.transform.translation.y);
+		kf_z_.initializePosition(msg.transform.translation.z);
+	}
 	new_measurement_ = true;
 	raw_transform_ = msg;
-	printf("image\n");
 }
 
 int main(int argc, char **argv)
 {
 	ros::init(argc, argv, "PozyxEstimatorNode");
 	ros::NodeHandle private_node_handle_("~");
-	ros::NodeHandle n;
 	ros::Subscriber transform_input;
 
 	int rate;
@@ -44,8 +96,5 @@ int main(int argc, char **argv)
 	private_node_handle_.param("rate", rate, int(100));
 
 	PozyxEstimator pozyx_estimator(rate);
-
-	transform_input = n.subscribe("eaglered/position", 1, &PozyxEstimator::transformInputCbRos, &pozyx_estimator);
-
 	pozyx_estimator.run();
 }
